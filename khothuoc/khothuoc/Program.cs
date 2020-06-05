6265b6254cb7afa7527a8f9fde549c4038394468
tranhoaiuoc <57566754+tranhoaiuoc@users.noreply.github.com>
2020-06-05 21:07:00 +0700

    edit

--- khothuoc/khothuoc/Program.cs
+++ khothuoc/khothuoc/Program.cs
@@ -16,7 +16,7 @@ namespace khothuoc
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new NhapThuoc());
+            Application.Run(new Form1());
         }
     }
 }
